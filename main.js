9235d80f8841b5ea269896c3894e0c34b3262314
cgolinski <cgolinski@users.noreply.github.com>
2017-01-14 23:22:40 -0500

    switched to jQuery

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-/*
+
 $(document).ready(function() {
   var button = $('#theButton');
+	console.log('document ready jquery is working')
+
+	button.click(function() {
+	  var possibleAnswers = ['Yes. Bruh your mixtape is lit.', 'Nah bruh. Tell your children you love them and move to Mexico.', 'answer 3'];
+	  var answer = Math.floor(Math.random() * (possibleAnswers.length - 0) + 0);
+
+	  renderAnswer(possibleAnswers[answer]);
+	});
+
+	function renderAnswer(answer) {
+	  var response = $('#response');
+	  response.empty();
+	  response.append('<p>' + answer + '</p>');
+	}
+
 });
-*/
 
-var button = document.getElementById('theButton');
 
-button.onclick = function click() {
-  var possibleAnswers = ['Yes. Bruh your mixtape is lit.', 'Nah bruh. Tell your children you love them and move to Mexico.', 'answer 3'];
-  var answer = Math.floor(Math.random() * (possibleAnswers.length - 0) + 0);
-  //alert(possibleAnswers[answer]);
-  renderAnswer(possibleAnswers[answer]);
-}
-
-function renderAnswer(answer) {
-  document.getElementById('response').innerHTML = '<p>' + answer + '</p>';
-}
 
