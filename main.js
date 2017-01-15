b2a412fe985d77f606e6927cc02c23a69e2b1861
cgolinski <cgolinski@users.noreply.github.com>
2017-01-15 18:24:21 -0500

    styled page 1 & page 2

--- main.js
+++ main.js
@@ -4,16 +4,22 @@ $(document).ready(function() {
 	console.log('document ready jquery is working')
 
 	button.click(function() {
-	  var possibleAnswers = ['Yes. Bruh your mixtape is lit.', 'Nah bruh. Tell your children you love them and move to Mexico.', 'answer 3'];
+	  var possibleAnswers = ['Yes. Bruh your mixtape is lit.', 'Nah bruh. Tell your children you love them and move to Mexico.', 'answer 3', 'Nah bruh. Tell your children you love them and move to Mexico.Nah bruh. Tell your children you love them and move to Mexico.Nah bruh. Tell your children you love them and move to Mexico.Nah bruh. Tell your children you love them and move to Mexico.'];
 	  var answer = Math.floor(Math.random() * (possibleAnswers.length - 0) + 0);
 
 	  renderAnswer(possibleAnswers[answer]);
+	  moveQuestion();
 	});
 
 	function renderAnswer(answer) {
-	  var response = $('#response');
+	  var response = $('#response p');
 	  response.empty();
-	  response.append('<p>' + answer + '</p>');
+	  response.append(answer);
+	}
+
+	function moveQuestion() {
+		var body = $('body');
+		body.addClass('withAnswer');
 	}
 
 });
